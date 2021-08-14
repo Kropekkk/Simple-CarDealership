7563c635d34e69f2aba46bb0867bf59b68283bc6
Kropekkk <Kropekkk@users.noreply.github.com>
2021-08-14 13:44:53 +0200

    Site design

--- MainApp/models.py
+++ MainApp/models.py
@@ -4,6 +4,8 @@ class Offer(models.Model):
     brand = models.CharField(max_length=20)
     model = models.CharField(max_length=20)
     generation = models.CharField(max_length=8,blank=True)
+    year = models.CharField(max_length=4)
+    distance = models.CharField(max_length=7)
     price=models.CharField(max_length=7)
     mainimage = models.ImageField(upload_to='images')
     description=models.CharField(max_length=200)
